66f97ad02e4244096411c7dcef69c0a415699292
ikechukz <ikechukz@gmail.com>
2018-09-27 11:15:09 +0100

    first commit

---
+++ app/src/main/java/com/ezeagu/android/iykessalon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ezeagu.android.iykessalon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
